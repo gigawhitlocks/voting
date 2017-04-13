0280d8bfd3176ecab5629ac478ae5430dfb8bb64
root <root@s88990.wholesaleinternet.net>
2017-04-13 09:56:11 -0500

    description

--- schema.sql
+++ schema.sql
@@ -19,10 +19,7 @@ INSERT INTO books (title, author, link, description, id)
 VALUES('His Dark Materials','Philip Pullman', '', '', 'c1995ad8-407e-ca51-75cf-5642f84e34c4');
 
 INSERT INTO books (title, author, link, description, id)
-VALUES('The Dispossessed','Ursula Leguin', '', '', '12a71276-e9d5-c177-89de-d1db224eae49');
-
-INSERT INTO books (title, author, link, description, id)
-VALUES('Norse Mythology anything','Neil Gaiman', '', '', '7331bf92-47ac-cf32-662c-5ed34a695a2d');
+VALUES('Norse Mythology','Neil Gaiman', '', '', '7331bf92-47ac-cf32-662c-5ed34a695a2d');
 
 INSERT INTO books (title, author, link, description, id)
 VALUES('Dirk Gentlys Holistic Detective Agency & The Long Dark Tea Time of the Soul','Douglas Adams', '', '', 'e42583a0-86f1-611b-5ef3-9d5fb85e9be1');
@@ -40,10 +37,10 @@ INSERT INTO books (title, author, link, description, id)
 VALUES('The Postman', 'David Brin', '', '', '82b25955-c276-7e29-edaa-57adb2f35117');
 
 INSERT INTO books (title, author, link, description, id)
-VALUES('Fear and Loathing and The Rum Diaries', 'Hunter S Thompson', '', '', '9cba1258-65d7-ef10-2b2a-cb6c887ab52f');
+VALUES('Fear and Loathing', 'Hunter S Thompson', '', '', '9cba1258-65d7-ef10-2b2a-cb6c887ab52f');
+
+INSERT INTO books (title, author, link, description, id)
+VALUES('The Rum Diaries', 'Hunter S Thompson', '', '', '8cba1258-65d7-ef10-2b2a-cb6c887ab52f');
 
 INSERT INTO books (title, author, link, description, id)
 VALUES('Distress', 'Greg Egan', '', '', 'b66ca302-bebd-331f-cdfe-da7849888d21');
-
-INSERT INTO books (title, author, link, description, id)
-VALUES('Childhoods End', 'Arthur C. Clarke', '', '', '2b6a4122-de40-4b3b-ef3b-7a24fd89d569');
